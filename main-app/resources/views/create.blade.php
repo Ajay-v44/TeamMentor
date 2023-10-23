01a9b44d032e816d7ffa79be3244f481feb2713a
Ajay v <vajay4834@gmail.com>
2023-10-23 11:35:33 +0530

    added error messages below input elements

--- main-app/resources/views/create.blade.php
+++ main-app/resources/views/create.blade.php
@@ -1,12 +1,13 @@
 @extends('layout.app')
 @section('content')
-@if($errors->any())
+<!-- To display error messages in a separate div -->
+<!-- @if($errors->any())
 @foreach($errors->all() as $error)
 <div class="alert alert-danger">
     {{$error}}
 </div>
 @endforeach
-@endif
+@endif -->
 <div class="card">
     <div class="card-body">
         <p style="font-size:20px; font-weight:bold;">Create New Employee</p>
@@ -16,30 +17,38 @@
             <div class="form-group has-validation">
                 <label for="name">Name</label>
                 <input type="text" name="name" id="name" class="form-control" required>
-                <span class="invalid-feedback">
-                    <strong>Error</strong>
-                </span>
+                @if($errors->has('name'))
+                    <span class="invalid-feedback">
+                        <strong>{{$errors->first('name')}}</strong>
+                    </span>
+                @endif
             </div>
             <div class="form-group has-validation">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" class="form-control" required>
-                <span class="invalid-feedback">
-                    <strong>Error</strong>
-                </span>
+                @if($errors->has('email'))
+                    <span class="invalid-feedback">
+                        <strong>{{$errors->first('email')}}</strong>
+                    </span>
+                @endif
             </div>
             <div class="form-group has-validation">
                 <label for="joining_date">Joining date</label>
                 <input type="date" name="joining_date" id="joining_date" class="form-control" required>
-                <span class="invalid-feedback">
-                    <strong class="invalid-feedback">Error</strong>
-                </span>
+                @if($errors->has('joining_date'))
+                    <span class="invalid-feedback">
+                        <strong>{{$errors->first('joining_date')}}</strong>
+                    </span>
+                @endif
             </div>
             <div class="form-group has-validation">
                 <label for="joining_salary">Joining salary</label>
                 <input type="number" name="salary" id="joining_salary" class="form-control" required>
+                @if($errors->has('salary'))
                 <span class="invalid-feedback">
-                    <strong>Error</strong>
+                    <strong>{{$errors->first('salary')}}</strong>
                 </span>
+            @endif
             </div>
             <div class="form-group has-validation">
                 <label for="is_active">Active</label><br>

832cf228e65f4f3c4bc12ce4797c39b2020385e2
Ajay v <vajay4834@gmail.com>
2023-10-23 11:59:20 +0530

    validation completed

--- main-app/resources/views/create.blade.php
+++ main-app/resources/views/create.blade.php
@@ -16,7 +16,7 @@
 
             <div class="form-group has-validation">
                 <label for="name">Name</label>
-                <input type="text" name="name" id="name" class="form-control" required value="{{old('name')}}">
+                <input type="text" name="name" id="name" class="form-control {{$errors->has('name')?'is-invalid':''}}" required value="{{old('name')}}">
                 @if($errors->has('name'))
                     <span class="invalid-feedback">
                         <strong>{{$errors->first('name')}}</strong>
@@ -25,7 +25,7 @@
             </div>
             <div class="form-group has-validation">
                 <label for="email">Email</label>
-                <input type="email" name="email" id="email" class="form-control" required value="{{old('email')}}" >
+                <input type="email" name="email" id="email" class="form-control {{$errors->has('email')?'is-invalid':''}}" required value="{{old('email')}}" >
                 @if($errors->has('email'))
                     <span class="invalid-feedback">
                         <strong>{{$errors->first('email')}}</strong>
@@ -34,7 +34,7 @@
             </div>
             <div class="form-group has-validation">
                 <label for="joining_date">Joining date</label>
-                <input type="date" name="joining_date" id="joining_date" class="form-control" required value="{{old('joining_date')}}">
+                <input type="date" name="joining_date" id="joining_date" class="form-control {{$errors->has('joining_date')?'is-invalid':''}}" required value="{{old('joining_date')}}">
                 @if($errors->has('joining_date'))
                     <span class="invalid-feedback">
                         <strong>{{$errors->first('joining_date')}}</strong>
@@ -43,7 +43,7 @@
             </div>
             <div class="form-group has-validation">
                 <label for="joining_salary">Joining salary</label>
-                <input type="number" name="salary" id="joining_salary" class="form-control" required value="{{old('salary')}}">
+                <input type="number" name="salary" id="joining_salary" class="form-control" {{$errors->has('salary')?'is-invalid':''}} required value="{{old('salary')}}">
                 @if($errors->has('salary'))
                 <span class="invalid-feedback">
                     <strong>{{$errors->first('salary')}}</strong>
@@ -52,7 +52,7 @@
             </div>
             <div class="form-group has-validation">
                 <label for="is_active">Active</label><br>
-                <input type="checkbox" name="is_active" id="is_active" value="1" required>
+                <input type="checkbox" name="is_active" id="is_active" class="{{$errors->has('is_active')?'is-invalid':''}}" value="1" required {{old('is_active')=='1'?'checked':''}}>
                 <span class="invalid-feedback">
                     <strong>Error</strong>
                 </span>
